18c1ffda92d44428908f938a0010a2099bd03268
SENECAGLOBAL\ramesh.avudurthi <ramesh.avudurthi@senecaglobal.com>
2022-11-10 16:25:24 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,7 @@
+// const d = new Date();
+// console.log(d)
+// console.log("Ramesh")
+let x = myFunction(4, 3);   // Function is called, return value will end up in x
+function myFunction(a, b) {
+  return a * b;             // Function returns the product of a and b
+}
